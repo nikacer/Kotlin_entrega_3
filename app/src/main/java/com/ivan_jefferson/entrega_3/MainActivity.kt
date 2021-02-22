0768f12f823d7de92fe9575a80cbdaa90b11f567
Nicolas Acero <nikolasg77@gmail.com>
2021-02-21 21:06:15 -0500

    interface creada

--- app/src/main/java/com/ivan_jefferson/entrega_3/MainActivity.kt
+++ app/src/main/java/com/ivan_jefferson/entrega_3/MainActivity.kt
@@ -1,23 +1,36 @@
 package com.ivan_jefferson.entrega_3
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.google.android.material.textfield.TextInputEditText
+
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
+        setContentView(R.layout.fragment_first)
+        val eventType = findViewById<Spinner>(R.id.event_type)
+        val list = resources.getStringArray(R.array.eventOptions)
+        val adapter = ArrayAdapter(this , android.R.layout.simple_spinner_item, list)
+        eventType.adapter = adapter
+
+
+
+
+
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -34,5 +47,15 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+
+    }
+
+    private fun showDatePickerDialog(){
+        val datePicker = DatePickerFragment{day,month,year -> onDateSelected(day,month,year)}
+        datePicker.show(supportFragmentManager, "datePicker")
+    }
+
+    fun onDateSelected(day:Int, month:Int, year:Int){
+
     }
 }
