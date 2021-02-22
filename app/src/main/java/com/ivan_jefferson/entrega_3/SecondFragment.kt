d83de13d5b40b80692a6abb5a510fe35289d22b2
Nicolas Acero <nikolasg77@gmail.com>
2021-02-22 00:32:51 -0500

    interface creada

--- app/src/main/java/com/ivan_jefferson/entrega_3/SecondFragment.kt
+++ app/src/main/java/com/ivan_jefferson/entrega_3/SecondFragment.kt
@@ -1,11 +1,13 @@
 package com.ivan_jefferson.entrega_3
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.fragment.findNavController
 
 /**
@@ -23,7 +25,14 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val params: Bundle? = arguments
+
+        val description = view.findViewById<EditText>(R.id.description)
+
+
+        if(arguments !== null) {
+            val params = arguments
+            if(params?.get("description") !== null) description.setText(params.get("description").toString())
+        }
 
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
