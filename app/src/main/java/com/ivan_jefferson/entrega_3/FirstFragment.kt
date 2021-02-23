f1d34bfdeef3522385aafe55b0d9c66482fa5dc9
Nicolas Acero <nikolasg77@gmail.com>
2021-02-23 00:34:46 -0500

    se agregan validaciones

--- app/src/main/java/com/ivan_jefferson/entrega_3/FirstFragment.kt
+++ app/src/main/java/com/ivan_jefferson/entrega_3/FirstFragment.kt
@@ -1,14 +1,20 @@
 package com.ivan_jefferson.entrega_3
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.CompletableFuture
+import java.util.regex.Pattern
+import kotlin.concurrent.timerTask
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -16,8 +22,8 @@ import androidx.navigation.fragment.findNavController
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
@@ -29,48 +35,82 @@ class FirstFragment : Fragment() {
         val buttonSave = view.findViewById<Button>(R.id.save)
         val switch = view.findViewById<Switch>(R.id.allDay)
         val name = view.findViewById<TextView>(R.id.name)
-        val EventType = view.findViewById<Spinner>(R.id.event_type)
+        val eventType = view.findViewById<Spinner>(R.id.event_type)
         val dateStart = view.findViewById<EditText>(R.id.dateStart)
         val hourStart = view.findViewById<EditText>(R.id.hourStart)
         val dateFinish = view.findViewById<EditText>(R.id.dateFinish)
         val hourFinish = view.findViewById<EditText>(R.id.hourFinish)
         val description = view.findViewById<EditText>(R.id.description)
-
-
-
         val dateStartTitle = view.findViewById<TextView>(R.id.dateStartTitle)
         val dateFinishtTitle = view.findViewById<TextView>(R.id.dateFinishTitle)
+        val errorsInput = view.findViewById<EditText>(R.id.errors)
+        val scroll = view.findViewById<ScrollView>(R.id.scroll)
+
+
+
 
             buttonSave.setOnClickListener {
                 var bundle:Bundle? = null
-                if(switch.isChecked){
-                   bundle = bundleOf(
-                       "description"  to  description.text,
-                       "dateStart" to dateStart.text,
-                       "allDay" to switch.isChecked,
-                       "name" to name.text
-                   )
+                 val errors = mutableListOf<String>()
+                val regex = "([01]?[0-9]|2[0-3]):[0-5][0-9]"
+                val p: Pattern = Pattern.compile(regex)
+
+
+                if (name.text.toString().trim().isEmpty()) errors.add("Debe tener un Nombre el Evento")
+                if(!validDate(dateStart.text.toString(), "dd/MM/yyyy")) errors.add("La fecha ${ if (!switch.isChecked ) "de Inicio" else ""} es incorrecta")
+
+
+                if(switch.isChecked  ){
+
+                    if(errors.isEmpty()){
+                        bundle = bundleOf(
+                                "description" to description.text,
+                                "dateStart" to dateStart.text,
+                                "allDay" to switch.isChecked,
+                                "name" to name.text,
+                                "eventType" to eventType.selectedItem
+                        )
+                    }
+
+
                 }else {
-                    bundle = bundleOf(
-                        "description" to description.text,
-                        "dateStart" to dateStart.text,
-                        "hourStart" to hourStart.text,
-                        "dateFinish" to dateFinish.text,
-                        "hourFinish" to hourFinish.text,
-                        "allDay" to switch.isChecked,
-                        "name" to name.text
-                    )
+                    if(!validDate(dateFinish.text.toString(), "dd/MM/yyyy")) errors.add("La fecha final es Incorrecta")
+                    if(p.matcher(hourStart.text.toString()).matches() || hourStart.text.isEmpty()) errors.add("La Hora de inicio es Incorrecta")
+                    if(p.matcher(hourFinish.text.toString()).matches() || hourFinish.text.isEmpty()) errors.add("La Hora Final es Incorrecta")
+
+
+
+                    if(errors.isEmpty()){
+                        bundle = bundleOf(
+                                "description" to description.text,
+                                "dateStart" to dateStart.text,
+                                "hourStart" to hourStart.text,
+                                "dateFinish" to dateFinish.text,
+                                "hourFinish" to hourFinish.text,
+                                "allDay" to switch.isChecked,
+                                "name" to name.text,
+                                "eventType" to eventType.selectedItem
+                        )
+                    }
                 }
 
                 if(bundle !== null) {
+                    errorsInput.isVisible = false
                     findNavController().navigate(
-                        R.id.action_FirstFragment_to_SecondFragment,
-                        bundle
+                            R.id.action_FirstFragment_to_SecondFragment,
+                            bundle
                     )
+                }else{
+                    errorsInput.isVisible=true
+                    errorsInput.setText(errors.joinToString("\n"))
+
+                    Timer().schedule(timerTask { scroll.smoothScrollTo(0,30000) },200)
+
                 }
         }
 
         switch.setOnClickListener {
+            errorsInput.isVisible = false
             if(switch.isChecked){
                 hourStart.isVisible = false
                 dateFinishtTitle.isVisible =false
@@ -87,14 +127,21 @@ class FirstFragment : Fragment() {
         }
 
 
-        val eventType = view.findViewById<Spinner>(R.id.event_type)
-        //val list = resources.getStringArray(R.array.eventOptions)
-
-        val list = ArrayList<String>()
-
-        //val adapter = ArrayAdapter(this , android.R.layout.simple_spinner_item, list)
-        //eventType.adapter = adapter
     }
+
+
+    private fun validDate(date: String, format: String):Boolean{
+        var isValid = false
+        try {
+            SimpleDateFormat(format).parse(date)
+            isValid = true
+        }catch (e: Exception){
+            isValid=false
+        }
+
+        return isValid
+    }
+
 
 
 
--- app/src/main/java/com/ivan_jefferson/entrega_3/SecondFragment.kt
+++ app/src/main/java/com/ivan_jefferson/entrega_3/SecondFragment.kt
@@ -8,6 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 
 /**
@@ -27,15 +30,50 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val description = view.findViewById<EditText>(R.id.description)
+        val dateStart = view.findViewById<EditText>(R.id.dateStart)
+        val hourStart = view.findViewById<EditText>(R.id.hourStart)
+        val dateFinish = view.findViewById<EditText>(R.id.dateFinish)
+        val hourFinish = view.findViewById<EditText>(R.id.hourFinish)
+        val eventType = view.findViewById<EditText>(R.id.event_type)
+        val eventName = view.findViewById<EditText>(R.id.eventName)
+        val allDay = view.findViewById<Switch>(R.id.allDay)
+
+        val titleDateStart = view.findViewById<TextView>(R.id.dateStartTitle)
+        val titleDateFinish = view.findViewById<TextView>(R.id.dateFinishTitle)
 
 
         if(arguments !== null) {
-            val params = arguments
-            if(params?.get("description") !== null) description.setText(params.get("description").toString())
+            val params:Bundle? = arguments
+
+            actionView("description", description,params)
+            //actionView("dateStart",dateStart,params)
+            actionView("hourStart",hourStart,params)
+            //actionView("dateFinish",dateFinish,params)
+           val respHorFinish = actionView("hourFinish", hourFinish, params)
+            //actionView("eventType",eventType,params)
+            actionView("name",eventName,params)
+            if(params?.get("allDay" ) === "true"){
+                allDay.isChecked = true
+            }
+
+            if(respHorFinish){
+                titleDateFinish.isVisible = false
+            }
+
         }
 
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+    }
+
+    private fun actionView(param:String,element: EditText, params:Bundle?): Boolean{
+        var isNull = true
+        if(params?.get(param) !== null) {
+            isNull = false
+            element.setText(params.get(param).toString())
+        } else element.isVisible = false
+
+        return isNull
     }
 }
